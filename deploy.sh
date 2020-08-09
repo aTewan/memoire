5fa6236fd0e75af467dfbedc87596fe388cdc1c1
aTewan <tewan33@gmail.com>
2020-08-09 16:39:41 +0200

    màj du script pour déployer le cluster Kubernetes

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,11 @@ kubectl apply -f k8s/mongo-service.yaml
 
 kubectl apply -f k8s/trepo-monolithe-deployment.yaml
 kubectl apply -f k8s/trepo-monolithe-service.yaml
+
+kubectl apply -f k8s/db-users-deployment.yaml
+kubectl apply -f k8s/db-users-service.yaml
+
+kubectl apply -f k8s/mc-users-deployment.yaml
+kubectl apply -f k8s/mc-users-service.yaml
+
+kubectl apply -f k8s/nginx-service.yaml
